049f22f5d3423b3faec9feec1c11c0a7678d4520
toanVu244 <vutritoan244@gmail.com>
2024-10-26 19:49:10 +0700

    test

--- RolexAuthorizedStore_AndroidStudio-main/app/src/main/java/com/example/lab10/api/Product/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.lab10.api.Product;
-
-import com.example.lab10.api.APIClient;
-
-public class ProductRepository {
-    public static ProductService getProductService(){
-        return APIClient.getClient().create(ProductService.class);
-    }
-}
